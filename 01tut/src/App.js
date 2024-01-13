21f7e99c291d18e48e6cdb6767bbc47617d786c5
Bergin Jack <berginjack@Bergins-MacBook-Pro-6273.local>
2024-01-13 17:57:56 -0500

    Added the add item feature

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -7,46 +7,71 @@ import AddItem from './AddItem';
 function App() {
   const [items, setItems] = useState([
     {
-        id: 1,
-        checked: false,
-        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
     },
     {
-        id: 2,
-        checked: false,
-        item: "Item 2"
+      id: 2,
+      checked: false,
+      item: "Item 2"
     },
     {
-        id: 3,
-        checked: false,
-        item: "Item 3"
+      id: 3,
+      checked: false,
+      item: "Item 3"
     }
   ]);
 
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => 
-      item.id === id ? { ...item, checked: !item.checked} : item);
-    setItems(listItems) 
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  const [newItem, setNewItem] = useState('')
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems)
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems))
   }
 
-  const handleDelete = (id)=>{
-    const listItems = items.filter((item)=>item.id !== id);
-    setItems(listItems) 
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems)
+  }
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item);
+    setAndSaveItems(listItems)
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setAndSaveItems(listItems)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem)
+    // prevents fenceposting
+    setNewItem('');
+    console.log('submitted')
   }
 
   return (
     <div className="App">
-      <Header title="Grocery List"/>
-      <AddItem/>
+      <Header title="Grocery List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content
         items={items}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
 
-      <Footer length={items.length}/>
+      <Footer length={items.length} />
     </div>
   );
 }
